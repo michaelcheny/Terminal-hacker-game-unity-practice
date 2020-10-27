a327b5b824f12d8e3a46881c3623fe9434d2e98f
Michael Chen <michaelchengaming@gmail.com>
2020-10-27 16:19:00 -0700

    add menu options and main menu stuff

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -10,12 +10,10 @@ public class Hacker : MonoBehaviour
         ShowMainMenu();
     }
 
-    void ShowMainMenu()
+    void ShowMainMenu(string greeting = "Plankton")
     {
         Terminal.ClearScreen();
-        string greeting = "Hello doodoohead";
-        Terminal.WriteLine(greeting);
-        Terminal.WriteLine("Hello Master Plankton");
+        Terminal.WriteLine("Hello Master " + greeting);
         Terminal.WriteLine("");
         Terminal.WriteLine("What would you like to do today?");
         Terminal.WriteLine("");
@@ -24,6 +22,54 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("3. Hack into Mr. Crab's bank account.");
         Terminal.WriteLine("");
         Terminal.WriteLine("Enter your selection: ");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (input == "1")
+        {
+            ContinueOption1();
+        }
+        else if (input == "2")
+        {
+            ContinueOption2();
+        }
+        else if (input == "3")
+        {
+            ContinueOption3();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid option...");
+        }
+    }
+
+    void ContinueOption1()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Raviolli raviolli show me the Formioulli!");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter 'menu' to return to menu");
+    }
+
+    void ContinueOption2()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Logging into Squidward's Onlyfans account...");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter 'menu' to return to menu");
+    }
+
+    void ContinueOption3()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Entering Mr. Crab's bank pin...");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter 'menu' to return to menu");
     }
 
     // Update is called once per frame
