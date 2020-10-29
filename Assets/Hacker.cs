f41511ae08b2932e31a23af5be8c3b0d007b2687
Michael Chen <michaelchengaming@gmail.com>
2020-10-28 22:21:17 -0700

    refactor and add extra level for the game, add constants and stuffs

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+
+    // Game Config
+    const string menuHint = "Type 'menu' to return to main menu";
+    string[] level1Password = { "patty", "pickles", "lettuce", "tomatoes", "cheese", "ketchup", "mustard", "onions" };
+    string[] level2Password = { "handsome", "clarinet", "tentacles", "misery", "cashier" };
+    string[] level3Password = { "money", "dollar", "greedy", "payday" };
 
     // Game State
     int level;
@@ -52,71 +58,128 @@ public class Hacker : MonoBehaviour
 
     void RunMainMenu(string input)
     {
-        if (input == "1")
+
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+
+        if (isValidLevelNumber)
         {
-            level = 1;
-            password = "toe nails";
-            StartGame();
+            level = int.Parse(input);
+            AskForPassword();
         }
-        else if (input == "2")
-        {
-            level = 2;
-            password = "clarinet";
-            StartGame();
-        }
-        else if (input == "3")
-        {
-            level = 3;
-            password = "money";
-            StartGame();
-        }
+
         else
         {
             Terminal.WriteLine("Please choose a valid option...");
+            Terminal.WriteLine(menuHint);
         }
 
     }
 
     void CheckUserGuess(string input)
     {
-        // input == password ? Terminal.WriteLine("Correct!") : Terminal.WriteLine("Wrong, you filthy animal!");
         if (input == password)
         {
-            Terminal.WriteLine("Correct!");
+            DisplayWinScreen();
         }
         else
         {
             Terminal.WriteLine("Wrong, you filthy animal!");
+            AskForPassword();
         }
     }
 
-    void StartGame()
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+        Terminal.WriteLine(menuHint);
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Raviolli raviolli show me the Formioulli!");
+                Terminal.WriteLine(@"
+   ,-.
+ _(*_*)_
+(_  o  _)
+  / o \
+ (_/ \_) 
+                ");
+                break;
+            case 2:
+                Terminal.WriteLine("Logging into Squidward's Onlyfans account...");
+                Terminal.WriteLine(@"
+        .--'''''''''--.
+     .'      .---.      '.
+    /    .-----------.    \
+   /        .-----.        \
+   |       .-.   .-.       |
+   |      /   \ /   \      |
+    \    | .-. | .-. |    /
+     '-._| | | | | | |_.-'
+         | '-' | '-' |
+          \___/ \___/
+       _.-'  /   \  `-._
+     .' _.--|     |--._ '.
+     ' _...-|     |-..._ '
+            |     |
+            '.___.'
+                ");
+                break;
+            case 3:
+                Terminal.WriteLine("Entering Mr. Crab's bank pin..."); ;
+                Terminal.WriteLine(@"
+   _      _
+  (<      >)
+   `O,99,O`
+  //-\__/-\\ 
+                ");
+                break;
+            default:
+                Debug.LogError("Invalid level reached");
+                break;
+        }
+    }
+
+    void AskForPassword()
     {
         currentScreen = Screen.Password;
         Terminal.ClearScreen();
-        Terminal.WriteLine("You have chosen level: " + level);
-        Terminal.WriteLine("");
 
-        if (level == 1)
-        {
-            Terminal.WriteLine("Raviolli raviolli show me the Formioulli!");
-        }
-        else if (level == 2)
-        {
-            Terminal.WriteLine("Logging into Squidward's Onlyfans account...");
-        }
-        else if (level == 3)
-        {
-            Terminal.WriteLine("Entering Mr. Crab's bank pin...");
-        }
+        SetRandomPassword();
+
+        Terminal.WriteLine("Enter the secret code: ");
+        Terminal.WriteLine("Hint: " + password.Anagram());
         Terminal.WriteLine("");
-        Terminal.WriteLine("Enter 'menu' to return to menu");
+        Terminal.WriteLine(menuHint);
+    }
+
+    void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = level1Password[Random.Range(0, level1Password.Length)];
+                break;
+            case 2:
+                password = level2Password[Random.Range(0, level2Password.Length)];
+                break;
+            case 3:
+                password = level3Password[Random.Range(0, level3Password.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
 }
