32d0ba51be15aa681c020a83e8c3f352d086f658
Michael Chen <michaelchengaming@gmail.com>
2020-10-28 17:30:39 -0700

    add password guessing in game

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -7,6 +7,7 @@ public class Hacker : MonoBehaviour
 
     // Game State
     int level;
+    string password;
     enum Screen { MainMenu, Password, Win }
 
     Screen currentScreen;
@@ -43,6 +44,10 @@ public class Hacker : MonoBehaviour
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckUserGuess(input);
+        }
     }
 
     void RunMainMenu(string input)
@@ -50,16 +55,19 @@ public class Hacker : MonoBehaviour
         if (input == "1")
         {
             level = 1;
+            password = "toe nails";
             StartGame();
         }
         else if (input == "2")
         {
             level = 2;
+            password = "clarinet";
             StartGame();
         }
         else if (input == "3")
         {
             level = 3;
+            password = "money";
             StartGame();
         }
         else
@@ -67,6 +75,19 @@ public class Hacker : MonoBehaviour
             Terminal.WriteLine("Please choose a valid option...");
         }
 
+    }
+
+    void CheckUserGuess(string input)
+    {
+        // input == password ? Terminal.WriteLine("Correct!") : Terminal.WriteLine("Wrong, you filthy animal!");
+        if (input == password)
+        {
+            Terminal.WriteLine("Correct!");
+        }
+        else
+        {
+            Terminal.WriteLine("Wrong, you filthy animal!");
+        }
     }
 
     void StartGame()
